888dfbb31ca5aef4e075f7c7340926b6f9de78ad
YashP-01 <yashpatelv21@gmail.com>
2026-01-30 19:57:00 +0530

    Fix stores page spacing: increase top margin between navbar and heading

--- app/stores/page.tsx
+++ app/stores/page.tsx
@@ -103,7 +103,7 @@ export default function StoresPage() {
   return (
     <div className="min-h-screen">
       <Navbar />
-      <div className="pt-20 pb-16">
+      <div className="pt-32 pb-16">
         <div className="container mx-auto px-4">
           <motion.div
             initial={{ opacity: 0, y: 20 }}
